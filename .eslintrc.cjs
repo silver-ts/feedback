c3b4b171eddd74d6d759f8ddbe49e5b3d93cdc42
silvertechsavvy <silvertechsavvy@gmail.com>
2022-06-23 13:54:50 +0300

    chore: update vite, eslint & prettier settings

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,22 +1,25 @@
 /* eslint-env node */
-require("@rushstack/eslint-patch/modern-module-resolution");
+require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
-  "root": true,
-  "extends": [
-    "plugin:vue/vue3-essential",
-    "eslint:recommended",
-    "@vue/eslint-config-prettier"
-  ],
-  "overrides": [
+  root: true,
+  env: {
+    es6: true,
+    node: true,
+    browser: true,
+  },
+  extends: ['plugin:vue/vue3-essential', 'eslint:recommended', '@vue/eslint-config-prettier'],
+  overrides: [
     {
-      "files": [
-        "**/__tests__/*.{cy,spec}.{js,ts,jsx,tsx}",
-        "cypress/e2e/**.{cy,spec}.{js,ts,jsx,tsx}"
+      files: [
+        '**/__tests__/*.{cy,spec}.{js,ts,jsx,tsx}',
+        'cypress/e2e/**.{cy,spec}.{js,ts,jsx,tsx}',
       ],
-      "extends": [
-        "plugin:cypress/recommended"
-      ]
-    }
-  ]
+      extends: ['plugin:cypress/recommended'],
+    },
+    {
+      files: ['*.html'],
+      processor: 'vue/.vue',
+    },
+  ],
 }
--- cypress.config.js
+++ cypress.config.js
@@ -3,13 +3,13 @@ const { defineConfig } = require('cypress')
 module.exports = defineConfig({
   e2e: {
     specPattern: 'cypress/e2e/**/*.{cy,spec}.{js,jsx,ts,tsx}',
-    baseUrl: 'http://localhost:4173'
+    baseUrl: 'http://localhost:4173',
   },
   component: {
     specPattern: 'src/**/__tests__/*.{cy,spec}.{js,ts,jsx,tsx}',
     devServer: {
       framework: 'vue',
-      bundler: 'vite'
-    }
-  }
+      bundler: 'vite',
+    },
+  },
 })
