c0e2d5b50e72e15ef123239a31e587fb9b8768ef
silvertechsavvy <silvertechsavvy@gmail.com>
2022-06-24 15:52:18 +0300

    build: add icons lib

--- src/main.js
+++ src/main.js
@@ -1,9 +1,16 @@
 import { createApp } from 'vue'
-import App from './App.vue'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faGithub } from '@fortawesome/free-brands-svg-icons'
+
 import router from './router'
+import App from './App.vue'
 
 const app = createApp(App)
 
-app.use(router)
+// Fontawesome icons
+library.add(faGithub)
+app.component('font-awesome-icon', FontAwesomeIcon)
 
+app.use(router)
 app.mount('#app')
